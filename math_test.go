d5bc0367e2fda9ef6ad68ee037a383edf837c02a
pedro-hca <pedroandradecontato@hotmail.com>
2024-02-29 10:09:44 -0300

    fix: fix test that was not passing on sum function

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	total := Sum(15, 15)
+	total := sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Sum result invalid: result %d. value expected: %d", total, 30)
